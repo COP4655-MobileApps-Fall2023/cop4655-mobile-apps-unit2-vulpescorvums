bdde6e6ef5018b031be9e6a336e2be88ce4acd27
Ky <kmallonn2021@fau.edu>
2023-09-12 19:45:00 -0400

    Added code and main view controller

--- Unit 2 Project/Unit 2 Project/DetailViewcontroller.swift
+++ Unit 2 Project/Unit 2 Project/DetailViewcontroller.swift
@@ -1,22 +1,31 @@
+import Nuke
 import UIKit
 
 class DetailViewcontroller: UIViewController {
-
+    
+    var movie: Movie!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        /*Nuke.loadImage(with: movie.posterImage, into: posterImageView)
+         
+         // Set labels with the associated movie values.
+         movieNameLabel.text = movie.movieName
+         movieDetailLabel.text = movie.movieDetail
+         
+         }*/
+        
+        
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
